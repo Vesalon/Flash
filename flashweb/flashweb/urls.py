7b6998a48eaec868cd6a6669d559594791361427
Vesselin Grantcharov <vess13@gmail.com>
2015-06-19 21:19:05 -0400

    added django authenticate views

--- flashweb/flashweb/urls.py
+++ flashweb/flashweb/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
+
 urlpatterns = [
+    '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1/',  include(router.urls)),
+
+    url('^.*$', IndexView.as_view(), name='index'),
 ]
