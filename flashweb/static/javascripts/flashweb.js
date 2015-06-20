fd01c73d809ffba24c4928d61aaaf30af5d21071
ninagran <ninagran@gmail.com>
2015-06-20 14:36:59 -0400

    csrf

--- flashweb/static/javascripts/flashweb.js
+++ flashweb/static/javascripts/flashweb.js
@@ -15,5 +15,20 @@ angular
       .module('flashweb.routes', ['ngRoute']);
 
     angular
-  .module('flashweb.config', []);
+        .module('flashweb.config', []);
   })();
+
+  angular
+  .module('thinkster')
+  .run(run);
+
+run.$inject = ['$http'];
+
+/**
+* @name run
+* @desc Update xsrf $http headers to align with Django's defaults
+*/
+function run($http) {
+  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $http.defaults.xsrfCookieName = 'csrftoken';
+}
