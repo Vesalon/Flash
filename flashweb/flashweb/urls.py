d5b1e868b226de7100dd80607249534f285bc114
Vesselin Grantcharov <vess13@gmail.com>
2015-06-26 18:25:55 -0400

    fixed error of login and register not showing up

--- flashweb/flashweb/urls.py
+++ flashweb/flashweb/urls.py
@@ -26,10 +26,10 @@ router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 router.register(r'haps', HapViewSet)
 
-accounts_router = routers.NestedSimpleRouter(
-    router, r'accounts', lookup='account'
-)
-accounts_router.register(r'haps', AccountHapsViewSet)
+# accounts_router = routers.NestedSimpleRouter(
+#     router, r'accounts', lookup='account'
+# )
+# accounts_router.register(r'haps', AccountHapsViewSet)
 
 urlpatterns = [
     # '',
@@ -37,8 +37,8 @@ urlpatterns = [
     url(r'^api/v1/',  include(router.urls)),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
-    url(r'^api/v1/', include(router.urls)),
-    url(r'^api/v1/', include(accounts_routher.urls)),
+    # url(r'^api/v1/', include(router.urls)),
+    # url(r'^api/v1/', include(accounts_routher.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
 ]
