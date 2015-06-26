94dc34da0f50cd98c1a3ec021d199ef797a86257
Vesselin Grantcharov <vess13@gmail.com>
2015-06-26 02:44:44 -0400

    leave additions and changes to angular here - breaks urls

---
+++ flashweb/static/javascripts/haps/directives/hap.directive.js
@@ -0,0 +1,32 @@
+/**
+* Hap
+* @namespace flashweb.hap.directives
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('flashweb.hap.directives') //MIGHT BE flashweb.haps,
+                                       //not flashweb.hap
+    .directive('hap', hap);
+
+  /**
+  * @namespace hap
+  */
+  function hap() {
+    /**
+    * @name directive
+    * @desc The directive to be returned
+    * @memberOf flashweb.hap.directives.Hap
+    */
+    var directive = {
+      restrict: 'E',
+      scope: {
+        post: '='
+      },
+      templateUrl: '/static/templates/haps/hap.html'
+    };
+
+    return directive;
+  }
+})();
