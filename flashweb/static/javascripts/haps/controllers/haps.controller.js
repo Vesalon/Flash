4868f3d0845f50ea7130fedb75ebb201d4ef931e
Vesselin Grantcharov <vess13@gmail.com>
2015-06-27 15:49:34 -0400

    show all haps

--- flashweb/static/javascripts/haps/controllers/haps.controller.js
+++ flashweb/static/javascripts/haps/controllers/haps.controller.js
@@ -32,67 +32,6 @@
       $scope.$watch(function () { return $(window).width(); }, render);
     }
 
-
-    /**
-    * @name calculateNumberOfColumns
-    * @desc Calculate number of columns based on screen width
-    * @returns {Number} The number of columns containing Haps
-    * @memberOf flashweb.haps.controllers.HapsControllers
-    */
-    function calculateNumberOfColumns() {
-      var width = $(window).width();
-
-      if (width >= 1200) {
-        return 4;
-      } else if (width >= 992) {
-        return 3;
-      } else if (width >= 768) {
-        return 2;
-      } else {
-        return 1;
-      }
-    }
-
-
-    /**
-    * @name approximateShortestColumn
-    * @desc An algorithm for approximating which column is shortest
-    * @returns The index of the shortest column
-    * @memberOf flashweb.haps.controllers.HapsController
-    */
-    function approximateShortestColumn() {
-      var scores = vm.columns.map(columnMapFn);
-
-      return scores.indexOf(Math.min.apply(this, scores));
-
-
-      /**
-      * @name columnMapFn
-      * @desc A map function for scoring column heights
-      * @returns The approximately normalized height of a given column
-      */
-      function columnMapFn(column) {
-        var lengths = column.map(function (element) {
-          return element.content.length;
-        });
-
-        return lengths.reduce(sum, 0) * column.length;
-      }
-
-
-      /**
-      * @name sum
-      * @desc Sums two numbers
-      * @params {Number} m The first number to be summed
-      * @params {Number} n The second number to be summed
-      * @returns The sum of two numbers
-      */
-      function sum(m, n) {
-        return m + n;
-      }
-    }
-
-
     /**
     * @name render
     * @desc Renders Haps into columns of approximately equal height
@@ -102,17 +41,20 @@
     */
     function render(current, original) {
       if (current !== original) {
-        vm.columns = [];
+        vm.columns = [[],];
 
-        for (var i = 0; i < calculateNumberOfColumns(); ++i) {
-          vm.columns.push([]);
-        }
+        // for (var i = 0; i < calculateNumberOfColumns(); ++i) {
+        //   vm.columns.push([]);
+        // }
 
         for (var i = 0; i < current.length; ++i) {
-          var column = approximateShortestColumn();
+          var column = 0;
+        //   var column = approximateShortestColumn();
+        //   console.log(column);
 
           vm.columns[column].push(current[i]);
         }
+        console.log(vm.columns);
       }
     }
   }
