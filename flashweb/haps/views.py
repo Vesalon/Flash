ce3401b9c16f38295453da52cbbf63d99112d489
Vesselin Grantcharov <vess13@gmail.com>
2015-07-03 01:08:33 -0400

    add settings

--- flashweb/haps/views.py
+++ flashweb/haps/views.py
@@ -10,37 +10,20 @@ class HapViewSet(viewsets.ModelViewSet):
     queryset = Hap.objects.order_by('-time')
     serializer_class = HapSerializer
     def get_permissions(self):
-        print('~~~~~~' + self.request.method + '~~~~~~')
-        print(self.request.data)
         if self.request.method in permissions.SAFE_METHODS:
             return (permissions.AllowAny(),)
-        x = (permissions.IsAuthenticated(),
+        return (permissions.IsAuthenticated(),
                 IsAuthorOfHap(),
             )
-        print(x[1])
-        return x
-
-    # def perform_create(self, serializer):
-    #     print('^^^^^^' + self.request.method + '^^^^^^')
-    #     instance = serializer.save(organizer=self.request.user)
-    #
-    #     return super(HapViewSet, self).perform_create(serializer)
 
     def create(self, request):
-        print('^^^^^^' + self.request.method + '^^^^^^')
-        try:
-            serializer = self.serializer_class(data=request.data)
+        serializer = self.serializer_class(data=request.data)
 
-            print('hello1')
-            if serializer.is_valid():
-                Hap.objects.create(organizer=request.user,
-                    **serializer.validated_data)
-                print('hello2')
-                return Response(serializer.validated_data,
-                        status=status.HTTP_201_CREATED)
-        except Exception, e:
-            print('---------------')
-            print(e)
+        if serializer.is_valid():
+            Hap.objects.create(organizer=request.user,
+                **serializer.validated_data)
+            return Response(serializer.validated_data,
+                    status=status.HTTP_201_CREATED)
 
 
 class AccountHapsViewSet(viewsets.ViewSet):
