bcfa591f241ada228fb8c52acac485737e178a6f
ninagran <ninagran@gmail.com>
2015-07-08 22:03:27 -0400

    using again $http - list works

--- flashweb/static/javascripts/friends/controllers/friends.controller.js
+++ flashweb/static/javascripts/friends/controllers/friends.controller.js
@@ -5,66 +5,33 @@
 (function () {
   'use strict';
 
-
   angular
     .module('flashweb.friends.controllers')
     .controller('FriendsController', FriendsController);
 
-  FriendsController.$inject = ['$scope',  'Authentication', 'Friends'];
+  FriendsController.$inject = ['$scope', 'Friends'];
 
   /**
   * @namespace FriendsController
   */
-  function FriendsController($scope, Authentication, Friends) {
+  function FriendsController($scope, Friends) {
     var vm = this;
-    vm.isAuthenticated = Authentication.isAuthenticated();
+
     vm.list = [];
     //vm.list = ["Tom", "Dick", "Harry"];
     //vm.list = $scope.list;
-    console.log('before calling all');
-    //vm.list = Friends.all();
-    /*
-    vm.list = Friends.all(function(callbackdata){
-      console.log(callbackdata);
-      console.log(vm.list);
-    });*/
-    Friends.query(function(response) {
-      vm.list = response;
-      console.log(response);
-      console.log('after calling query');
-    });
+    activate();
 
-    console.log('after calling all');
-  /*  Friends.all().$promise.then(function(data){
-      vm.list = data;
-    });*/
 
-  //  activate();
     /**
     * @name activate
     * @desc Actions to be performed when this controller is instantiated
     * @memberOf flashweb.friends.controllers.FriendsController
     */
     function activate() {
-      // used with $http
-      /*Friends.all().success(function(data) {
-        vm.list = data;
-      });*/
-
-    console.log('in friends activate() -- 1');
-
-    //used with $resource
-    //  $scope.friends = Friend.query();
-      vm.list = Friends.query();
-
-    //vm.list = Friends.all();
-
-/*
-      Friends.all().$promise.then(function(data) {
+      Friends.all().success(function(data) {
         vm.list = data;
       });
-*/
-      console.log('in friends activate() -- 2');
 
       $scope.$watchCollection(function () { return $scope.friends; }, render);
       $scope.$watch(function () { return $(window).width(); }, render);
