43b8af5056be4015149189df6c0d923c1ee4521c
Vesselin Grantcharov <vess13@gmail.com>
2015-07-10 16:03:59 -0400

    add guest model and migrations

--- flashweb/haps/models.py
+++ flashweb/haps/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from authentication.models import Account
+from friends.models import Friend
 
 class Hap(models.Model):
     organizer = models.ForeignKey(Account, related_name='organizer')
@@ -9,3 +10,10 @@ class Hap(models.Model):
     desc = models.TextField()
     time = models.DateTimeField()
     location = models.CharField(max_length=200)
+
+    guest_list = models.ManyToManyField(Friend, through='Guest')
+
+class Guest(models.Model):
+    friend = models.ForeignKey(Friend)
+    hap = models.ForeignKey(Hap)
+    comment = models.CharField(max_length=30)
