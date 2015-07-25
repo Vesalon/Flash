04443252dc28aaf438ede6ddc1c1993558c17a05
ninagran <ninagran@gmail.com>
2015-07-24 20:41:04 -0400

    checkin 2

---
+++ hapin_m/www/js/hapin.constants.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('hapin');
+
+  .constant('AUTH_EVENTS', {
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
+  })
+
+  .constant('USER_ROLES', {
+    admin: 'admin_role',
+    public: 'public_role'
+  })
+
+})();
