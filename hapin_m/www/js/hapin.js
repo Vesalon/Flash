066b35548b4316b272034fcf8eec78fb4b405abd
ninagran <ninagran@gmail.com>
2015-07-25 22:05:34 -0400

    displaying login form

--- hapin_m/www/js/hapin.js
+++ hapin_m/www/js/hapin.js
@@ -23,25 +23,25 @@ angular.module('hapin', [
   });
 
 
-  // $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
-  //
-  //   if ('data' in next && 'authorizedRoles' in next.data) {
-  //     var authorizedRoles = next.data.authorizedRoles;
-  //     if (!AuthService.isAuthorized(authorizedRoles)) {
-  //       event.preventDefault();
-  //       $state.go($state.current, {}, {reload: true});
-  //       $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
-  //     }
-  //   }
-  //
-  //   if (!AuthService.isAuthenticated()) {
-  //     if (next.name !== 'login') {
-  //       event.preventDefault();
-  //       $state.go('login');
-  //     }
-  //   }
+  $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
+
+    // if ('data' in next && 'authorizedRoles' in next.data) {
+    //   var authorizedRoles = next.data.authorizedRoles;
+    //   if (!Authentication.isAuthorized(authorizedRoles)) {
+    //     event.preventDefault();
+    //     $state.go($state.current, {}, {reload: true});
+    //     $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+    //   }
+    // }
+
+    if (!Authentication.isAuthenticated()) {
+      if (next.name !== 'login') {
+        event.preventDefault();
+        $state.go('login');
+      }
+    }
 
 
-  //});
+  });
 
 })
