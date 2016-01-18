21d3f9bcdfd22594d83c8fcc1ad998e0871d09e1
Vesselin Grantcharov <vess13@gmail.com>
2016-01-18 16:01:54 -0500

    make todo, no UI

--- c_hapin/www/js/haps/controllers/todo.controller.js
+++ c_hapin/www/js/haps/controllers/todo.controller.js
@@ -9,67 +9,26 @@
 
   function TodoController($scope, $state, Auth, Friends, $rootScope, $mdDialog) {
     var hi = this;
-    hi.isAuthenticated = Auth.isAuthenticated();
-    hi.friends = [];
-    hi.guestlist = [];
-    hi.cancelFilter = cancelFilter;
-    hi.showFilter = false;
-    hi.clearGuestlist = clearGuestlist;
-    hi.fireCancelEvent = fireCancelEvent;
-    hi.fireSelectedGuestsEvent = fireSelectedGuestsEvent;
+    hi.list = [];
 
-    activate();
-
-    function activate() {
-
-      if (hi.isAuthenticated) {
-        Friends.get()
-          .then(friendsSuccessFn, friendsErrorFn);
+    hi.add = function() {
+      console.log(hi.item);
+      if(hi.item) {
+        hi.list.push(hi.item);
+        hi.item = '';
       }
-
-      function friendsSuccessFn(data, status, headers, config) {
-        hi.friends = data.data;
-      }
-
-      function friendsErrorFn(data, status, headers, config) {
-        console.log(data.error);
-      }
-
-    }
-
-    hi.tester = function() {
-      console.log(hi.guestlist);
+      console.log(hi.list);
     }
 
     hi.toggle = function (item, list) {
       var idx = list.indexOf(item);
       if (idx > -1) list.splice(idx, 1);
       else list.push(item);
-
-      // don't fire it for every toggle; DONE button in the end will fire it
-      // hi.fireGuestSelectEvent(item);
-
     };
 
     hi.exists = function (item, list) {
       return list.indexOf(item) > -1;
     };
-
-    // hi.fireGuestSelectEvent = function(guest) {
-    //   // console.log(hi.guestlist);
-    //   $rootScope.$broadcast('guests:guest-selected', {
-    //     guestlist: hi.guestlist
-    //   });
-    // }
-
-    function cancelFilter(){
-      hi.showFilter = false;
-      $scope.friendsquery = "";
-    }
-
-    function clearGuestlist(){
-      hi.guestlist = [];
-    }
 
     function fireCancelEvent($event) {
       console.log('fired guests:cancel')
@@ -79,9 +38,6 @@
     };
 
     function fireSelectedGuestsEvent($event, place) {
-      // $scope.$emit('places:place-selected', {
-      //   place: place
-      // });
       console.log('fired guests:guests-selected')
       $rootScope.$broadcast('guests:guests-selected', {
         guestlist: hi.guestlist
